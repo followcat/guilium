79239490a2857db2b58fb685c3fb89bed449cff8
followcat <followcat@163.com>
2016-03-09 22:59:22 +0800

    Modify design notebook, split stub and it's validated engine.

--- doc/base_communication.py
+++ doc/base_communication.py
@@ -4,20 +4,23 @@ from graphviz import Digraph
 base_config = {
     'app': {
         'mata': 'Mata<BR/>validated SUT',
-        'validator': 'Validator',
         'collection': 'TestCollection<BR/>origin-outgoing',
     },
     'stub': {
-        'material': 'material',
-        'engine': 'simulation-engine',
+        'material': 'Material',
+        'validator': 'Validator',
+    },
+    'ValidatedSUT': {
+        'interface': 'Interface',
+        'engine': 'Engine',
     },
     'SUT': {
-        'interface': 'interface',
-        'engine': 'engine',
+        'interface': 'Interface',
+        'engine': 'Engine',
     },
     'SimulatedSUT': {
-        'material': 'material',
-        'engine': 'simulation-engine',
+        'material': 'Material',
+        'engine': 'Simulation-Engine',
     },
     'InValidator': {
         'validate': 'Validator',
@@ -32,11 +35,8 @@ base_config = {
 
 def basedesgin_graphviz(config=base_config):
     G = Digraph(name='pet-shop', node_attr={'shape': 'plaintext'})
-    G.node('app', u"""<
+    G.node('app', shape='egg', center='true', label="""<
     <TABLE>
-      <TR>
-          <TD PORT="validate" BGCOLOR="yellow" WIDTH="30" COLSPAN="9">%s</TD>
-      </TR>
       <TR>
           <TD PORT="collect" WIDTH="10" COLSPAN="3">%s</TD>
           <TD PORT="collect-out" BGCOLOR="grey" WIDTH="10" COLSPAN="3">%s</TD>
@@ -44,21 +44,30 @@ def basedesgin_graphviz(config=base_config):
       <TR>
           <TD WIDTH="30" COLSPAN="9" BGCOLOR="chartreuse">application</TD>
       </TR>
-    </TABLE>>""" % (config['app']['validator'],
-                    config['app']['mata'],
-                    config['app']['collection']),
-                    shape='egg',center='true')
+    </TABLE>>""" % (config['app']['mata'],
+                    config['app']['collection']))
     G.node('stub', shape='component', label="""<
     <TABLE>
       <TR>
-          <TD BGCOLOR="red" WIDTH="10" COLSPAN="3">%s</TD>
-          <TD PORT="engine" BGCOLOR="blue" WIDTH="10" COLSPAN="3">%s</TD>
+          <TD PORT="material" BGCOLOR="red" WIDTH="10" COLSPAN="3">%s</TD>
+          <TD PORT="validator" BGCOLOR="yellow" WIDTH="10" COLSPAN="3">%s</TD>
       </TR>
       <TR>
           <TD WIDTH="20" COLSPAN="6" BGCOLOR="chartreuse">stub</TD>
       </TR>
     </TABLE>>""" % (config['stub']['material'],
-                    config['stub']['engine']))
+                    config['stub']['validator']))
+    G.node('ValidatedSUT', shape='component', label="""<
+    <TABLE>
+      <TR>
+          <TD BGCOLOR="red" WIDTH="10" COLSPAN="3">%s</TD>
+          <TD BGCOLOR="blue" WIDTH="10" COLSPAN="3">%s</TD>
+      </TR>
+      <TR>
+          <TD WIDTH="20" COLSPAN="6" BGCOLOR="chartreuse">ValidatedSUT</TD>
+      </TR>
+    </TABLE>>""" % (config['ValidatedSUT']['interface'],
+                    config['ValidatedSUT']['engine']))
     G.node('SUT', shape='component', label="""<
     <TABLE>
       <TR>
@@ -102,10 +111,12 @@ def basedesgin_graphviz(config=base_config):
     </TABLE>>""" % (config['INCOM']['comm']), shape='box')
 
     G.edge("app:collect", "app:collect-out", color="grey")
+    G.edge("app:collect-out", "stub:material", color="grey", label="order")
+    G.edge("stub", "InCommunication", label="order", color="grey")
+    G.edge("stub", "ExCommunication", label="order", color="grey")
+    G.edge("ExCommunication", 'ValidatedSUT', dir="both", color="red:blue")
+    G.edge("ExCommunication", "stub:validator", label="storage", color="blue")
     G.edge("ExCommunication", 'SUT', dir="both", color="red:blue")
-    G.edge("stub", "app:validate", color="blue", label="storage")
-    G.edge("stub:engine", "InCommunication", label="order", color="grey")
-    G.edge("stub:engine", "ExCommunication", dir="both", label="order", color="grey:blue")
     G.edge("InCommunication", "SimulatedSUT", label="order", color="grey")
     G.edge("SimulatedSUT:engine", "InValidator", label="storage", color="blue")
 
