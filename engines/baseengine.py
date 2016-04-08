c02db2f6c01b1460f66e0651972a853e6e1f2332
jeff <jeff@debian83>
2016-04-08 14:35:28 +0800

    Use methods in _webdriver to start appium and get webdriver

--- engines/baseengine.py
+++ engines/baseengine.py
@@ -1,10 +1,6 @@
 import time
-import socket
-import threading
-import subprocess
 
-import appium.webdriver
-
+import _webdriver
 import engines.image
 
 class BaseEngine(object):
@@ -17,27 +13,19 @@ class BaseEngine(object):
     }
     def __init__(self, name):
         self.name = name
-        self.get_assign_port_lock = threading.Lock()
 
     def start(self):
-        self.port = self.pickfreeport()
-        self.bootstrap_port = self.pickfreeport()
-        self.chromedriver_port = self.pickfreeport()
-        command = ['appium', '--command-timeout=300',
-                   '--default-capabilities={"avd":"%s"}'%self.name,
-                   '-p', str(self.port),
-                   '-bp', str(self.bootstrap_port),
-                   '--chromedriver-port', str(self.chromedriver_port)]
-        self.p = subprocess.Popen(command,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+        self.p, port = _webdriver.start_appium_in_subprocess(avdname=self.name)
+
         while(True):
             outstr = self.p.stdout.readline()
             if "Appium REST http interface listener started on" in outstr:
                 break
+
+        command_executor = 'http://localhost:%s/wd/hub'% port
         try:
-            self.driver = appium.webdriver.Remote('http://localhost:%s/wd/hub'
-                             % self.port, self.desired_caps)
+            self.driver = \
+                _webdriver.get_webdriver(command_executor, self.desired_caps)
         except Exception as e:
             self.p.terminate()
             raise e
@@ -57,14 +45,6 @@ class BaseEngine(object):
         self.driver.get(url)
         image = engines.image.webviewfullscreen(self.driver)
         return image
-
-    def pickfreeport(self):
-        with self.get_assign_port_lock:
-            _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            _socket.bind(('127.0.0.1', 0))
-            addr, port = _socket.getsockname()
-            _socket.close()
-        return port
 
     def stop(self):
         self.driver.quit()
