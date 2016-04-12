75a466bbd26ce8ff6942db309b52a7be5bea47b1
jeff <jeff@debian83>
2016-04-12 12:44:32 +0800

    Add desktop chrome emulation as one of engine communications

---
+++ engine/communication/_desktop.py
@@ -0,0 +1,26 @@
+import selenium.webdriver
+import selenium.webdriver.chrome.options
+
+import engine.communication.base
+
+
+class Communication(engine.communication.base.Communication):
+
+    def __init__(self, name):
+        super(Communication, self).__init__(name)
+        mobile_emulation = { 
+            "deviceMetrics": {
+                "width": 480,
+                "height": 800,
+                "pixelRatio": 3.0},
+            "userAgent": "Mozilla/5.0 (Linux; Android 5.1.1; "
+                         "en-us; Nexus 5 Build/JOP40D) "}
+        self.options = selenium.webdriver.chrome.options.Options()
+        self.options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    def start(self):
+        self.driver = selenium.webdriver.Chrome(chrome_options=self.options)
+
+    def stop(self):
+        self.driver.quit()
+
