9cd56238c391c6887531561c26e09f505c6fc859
jeff <jeff@debian83>
2016-04-12 12:53:52 +0800

    Add more options detials in desktop communication

--- engine/communication/_desktop.py
+++ engine/communication/_desktop.py
@@ -14,7 +14,9 @@ class Communication(engine.communication.base.Communication):
                 "height": 800,
                 "pixelRatio": 3.0},
             "userAgent": "Mozilla/5.0 (Linux; Android 5.1.1; "
-                         "en-us; Nexus 5 Build/JOP40D) "}
+                         "en-us; Nexus 5 Build/JOP40D) "
+                         "AppleWebKit/535.19 (KHTML, like Gecko) "
+                         "Chrome/18.0.1025.166 Mobile Safari/535.19"}
         self.options = selenium.webdriver.chrome.options.Options()
         self.options.add_experimental_option("mobileEmulation", mobile_emulation)
 
