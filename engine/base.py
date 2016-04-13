b97b5e359888cb0fa8f084bd990a5059e6799465
jeff <jeff@debian83>
2016-04-13 11:42:54 +0800

    Select Matas for different test platform

--- engine/base.py
+++ engine/base.py
@@ -10,12 +10,21 @@ class Engine(object):
         'desktop':  engine.connector._desktop.DesktopConnector,
     }
 
+    mata_classes = {
+        'mobile':   [engine.matas.image.WebviewImageMata,
+                     engine.matas.dom.WebviewDomMata],
+        'desktop':  [engine.matas.image.DesktopImageMata,
+                     engine.matas.dom.DesktopDomMata]
+    }
+
     def __init__(self, name, config):
         self.name = name
         self.platform = config['platform']
         self.comm = self.connector_factory()
-        self.matas = [engine.matas.image.WebviewImageMata(),
-                      engine.matas.dom.WebviewDomMata()]
+        self.matas = self.mata_factory()
+
+    def mata_factory(self):
+        return [_cls() for _cls in self.mata_classes[self.platform]]
 
     def connector_factory(self):
         return self.connector_classes[self.platform](self.name)
