58432aeaae6f8b66ed2d5f3dd6895518a8e6ba2c
jeff <jeff@debian83>
2016-04-27 12:23:26 +0800

    Reorder the test list

--- test/test_plan.py
+++ test/test_plan.py
@@ -4,9 +4,9 @@ import core.test
 def urls():
 
     return ['http://localhost:5000/',
-            'http://localhost:5000/mod',
-            'http://localhost:5000/content',
-            'http://localhost:5000/samplemotor',
+            'http://localhost:5000/text',
+            'http://localhost:5000/textlayout',
+            'http://localhost:5000/image',
             'http://www.willendare.com/',
             'http://www.baidu.com/',
             'http://www.bing.com/',
