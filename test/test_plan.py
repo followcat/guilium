9912fb26c658e7dfc577d090f6b15126e3d76c38
jeff <jeff@debian83>
2016-05-20 15:34:12 +0800

    Extend url list in test_plan

--- test/test_plan.py
+++ test/test_plan.py
@@ -3,13 +3,30 @@ import core.test
 
 def urls():
 
-    return ['http://localhost:5000/',
-            'http://localhost:5000/text',
-            'http://localhost:5000/textlayout',
-            'http://localhost:5000/image',
-            'http://www.willendare.com/',
+    return ['http://10.0.0.119:5000/',
+            'http://10.0.0.119:5000/text',
+            'http://10.0.0.119:5000/textlayout',
+            'http://10.0.0.119:5000/image',
+            'http://10.0.0.119:5000/mismatch',
             'http://www.baidu.com/',
             'http://www.bing.com/',
+            'http://www.microsoft.com/zh-cn',
+            'http://www.msn.com',
+            'https://www.taobao.com',
+            'http://www.smzdm.com',
+            'http://www.sohu.com',
+            'http://wwww.sina.com.cn',
+            'http://www.chinarank.org.cn',
+            'http://www.jd.com',
+            'http://www.so.com',
+            'http://www.zhaopin.com',
+            'http://www.163.com',
+            'http://www.ifeng.com',
+            'http://www.51job.com',
+            'http://www.cntv.cn',
+            'http://www.chinanews.com',
+            'http://www.youku.com',
+            'http://www.ganji.com',
             ]
 
 def TestGenerator():
