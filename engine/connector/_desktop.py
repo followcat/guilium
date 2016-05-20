461cdb29f72864fe4a4a7eef228127342b924b63
jeff <jeff@debian83>
2016-05-20 15:32:23 +0800

    Add host and port in config file to identify remote test

--- engine/connector/_desktop.py
+++ engine/connector/_desktop.py
@@ -9,12 +9,24 @@ class DesktopConnector(engine.connector.base.Connector):
     def __init__(self, config):
         super(DesktopConnector, self).__init__(config)
         device_name = self.config['device name']
+        self.command_executor = None
+        if 'host' in self.config:
+            host = self.config['host']
+            if 'port' in self.config:
+                port = self.config['port']
+            else:
+                port = '4444'
+            self.command_executor = 'http://%s:%s/wd/hub'%(host, port)
         mobile_emulation = {"deviceName": device_name} 
         self.options = selenium.webdriver.chrome.options.Options()
         self.options.add_experimental_option("mobileEmulation", mobile_emulation)
 
     def start(self):
-        self.driver = selenium.webdriver.Chrome(chrome_options=self.options)
+        if self.command_executor is None:
+            self.driver = \
+                selenium.webdriver.Chrome(chrome_options=self.options)
+        else:
+            self.driver = selenium.webdriver.Remote(command_executor=self.command_executor, desired_capabilities=self.options.to_capabilities())
         self.driver.maximize_window()
 
     def stop(self):
