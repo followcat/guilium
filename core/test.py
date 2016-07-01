2e4fa5298941fbbeff5ae58b63683bc5cb287357
jeff <jeff@debian83>
2016-07-01 17:59:02 +0800

    Reconstruct the test generator from json test list

--- core/test.py
+++ core/test.py
@@ -1,43 +1,6 @@
-import functools
-
-
-def test_actions(test, driver):
-    url, actions = test
-    for action in actions:
-        if action == 'get' or action[0] == 'get':
-            driver.get(url)
-        else:
-            e = driver.find_element_by_xpath(action[1])
-            if action[0] == 'fill':
-                e.clear()
-                e.send_keys(action[-1])
-            elif action[0] == 'click':
-                e.click()
-
-def test_generator(urls):
-    for url in urls:
-        if isinstance(url, list):
-            url = tuple(url)
-            last_index = 0
-            for index, item in enumerate(url[1]):
-                if item == 'test':
-                    yield_test = (url[0], url[1][last_index: index])
-                    last_index = index + 1
-                    yield Test(yield_test), yield_test
-            if last_index < len(url[1]):
-                yield_test = (url[0], url[1][last_index:])
-                yield Test(yield_test), yield_test
-        else:
-            yield Test((url, ('get',))), url
-
 class Test:
-    def __init__(self, url):
-        self.test_actions = None
-        if isinstance(url, tuple):
-            self.test_actions = functools.partial(test_actions, url)
-            self.test_url = (url[0], self.test_actions)
-        else:
-            self.test_url = url
+    def __init__(self, test):
+        self.test = test
 
     def __call__(self, runtime, result=None, *args):
         """"""
--- test/test_plan.py
+++ test/test_plan.py
@@ -1,9 +1,56 @@
+import time
+import functools
+
 import json
 
 import core.test
 
 
+def test_actions(test, driver):
+    url, actions = test
+    for action in actions:
+        if action == 'get' or action[0] == 'get':
+            driver.get(url)
+        else:
+            e = driver.find_element_by_xpath(action[1])
+            if action[0] == 'fill':
+                e.clear()
+                e.send_keys(action[-1])
+            elif action[0] == 'click':
+                e.click()
+                time.sleep(3)
+
+
+class Test(core.test.Test):
+
+    def __init__(self, url):
+        self.test_actions = None
+        if isinstance(url, tuple):
+            self.test_actions = functools.partial(test_actions, url)
+            self.test_url = (url[0], self.test_actions)
+        else:
+            self.test_url = url
+
+
 def TestGenerator(test_list):
+    """
+        >>> import test.test_plan
+        >>> content = open('/tmp/test.json').read()
+        >>> tests = list(g)
+        >>> len(tests)
+    """
     urls = json.loads(test_list)
-    for test in core.test.test_generator(urls):
-        yield test
+    for url in urls:
+        if isinstance(url, list):
+            url = tuple(url)
+            last_index = 0
+            for index, item in enumerate(url[1]):
+                if item == 'test':
+                    yield_test = (url[0], url[1][last_index: index])
+                    last_index = index + 1
+                    yield Test(yield_test), yield_test
+            if last_index < len(url[1]):
+                yield_test = (url[0], url[1][last_index:])
+                yield Test(yield_test), yield_test
+        else:
+            yield Test((url, ('get',))), url
