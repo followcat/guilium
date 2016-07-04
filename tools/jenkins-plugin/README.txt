56776e96eaf1aec7fe97a5a4beabcf9aa31c1193
jeff <jeff@debian83>
2016-07-04 15:34:49 +0800

    Add jenkins plugin for pulish guilium as jenkins project

---
+++ tools/jenkins-plugin/README.txt
@@ -0,0 +1,2 @@
+This plugin is written for the internal user who will
+use Guilim project for UI automated testing.
---
+++ tools/jenkins-plugin/src/main/java/hudson/plugins/guilium_plugin/GuiliumBuilder.java
@@ -0,0 +1,145 @@
+package hudson.plugins.guilium_plugin;
+
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.model.Build;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.management.Descriptor;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link GuiliumBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(Build, Launcher, BuildListener)} method
+ * will be invoked. 
+ *
+ * @author ZhangQuanyun
+ */
+public class GuiliumBuilder extends Builder {
+
+
+    private final String cfile;
+    private final String tplan;
+
+    @DataBoundConstructor
+    public GuiliumBuilder(String cfile, String tplan) {
+        this.cfile = cfile;
+        this.tplan = tplan;
+    }
+
+    public String getCfile() {
+        return cfile;
+    }
+
+    public String getTplan() {
+        return tplan;
+    }
+
+    private boolean saveJsonFile(String content, String absPath) throws IOException{
+        File saveFile = new File(absPath);
+        if(!saveFile.exists()){
+            saveFile.createNewFile();
+        }
+        FileWriter fileWritter = new FileWriter(saveFile.getAbsolutePath());
+        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+        String json_content = null;
+        if (content.startsWith("[")) {
+            json_content = JSONArray.fromObject(content).toString();
+        }
+        else {
+            json_content = JSONObject.fromObject(content).toString();
+        }
+        bufferWritter.write(json_content);
+        bufferWritter.close();
+        return true;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        //save the config file and test file for python command run
+        String config_file = "/tmp/config.json";
+        String test_plan_file = "/tmp/test.json";
+        try {
+            saveJsonFile(cfile, config_file);
+            saveJsonFile(tplan, test_plan_file);
+        } catch(IOException e) {
+            listener.getLogger().println("Exception "+ e.getMessage());
+        }
+        String command = "python nose_plugin/plugin.py --with-guilium --config-file="+config_file+" --test-file="+test_plan_file+" test/test_plan.py -v";
+        //listener.getLogger().println(command);
+        boolean result = false;
+        try {
+            Process pro = Runtime.getRuntime().exec(command, null, new File("/home/jeff/Projects/uitest/guilium/guilium"));
+            pro.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(pro.getErrorStream()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+                if ("OK".equals(line)) {
+                    result = true;
+                }
+            }
+            String output = sb.toString();
+            listener.getLogger().println(output);
+        } catch (Exception e) {
+            listener.getLogger().println("Exception "+ e.getMessage());
+        } finally {
+            return result;
+        }
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        public DescriptorImpl() {
+            load();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Guilium UI Test";
+        }
+
+        @Override
+        public boolean isApplicable(Class type) {
+            return true;
+        }
+
+        @Override
+        public boolean configure(StaplerRequest staplerRequest, JSONObject json) throws FormException {
+            save();
+            return true; // indicate that everything is good so far
+        }
+    }
+}
